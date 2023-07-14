1f3ab32a7a3b147eea0ea124f6b3565dc3f8d8de
Your Name <savitri.kandgal@gmail.com>
2023-07-14 13:28:43 +0530

    Project

--- contactData-springMVC/src/com/xworkz/contactdata/constant/ContactDataConstant.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.contactdata.constant;
-
-public interface ContactDataConstant {
-
-	int MAX_FILE_SIZE=800000000;
-	String TEMP_FILE_LOCATION="C:\\Users\\DELL\\OneDrive\\Desktop\\JAVA_CLASS_2023\\tomcat-files\\temp";
-	String FILE_LOCATION="C:\\Users\\DELL\\OneDrive\\Desktop\\JAVA_CLASS_2023\\tomcat-files\\";
-	
-}
